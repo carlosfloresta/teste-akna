dc4fa6773a5ad9b10f96ec70ddd576149d25021e
Carlos Floresta <47724025+carlosfloresta@users.noreply.github.com>
2021-07-28 15:20:51 -0300

    ordenando conforme pedido

--- script.php
+++ script.php
@@ -7,14 +7,14 @@ function outputCSV($data)
 {
     $filename = "data_export_" . date("d-m-Y") . ".csv";
     $output = fopen($filename, "w");
+    uksort($data,"ordena_mes");
     foreach ($data as $key => $value) {
+        ksort($value);
         foreach ($value as $key2 => $value2) {
+
+            arsort($value2);
             foreach ($value2 as $key3 => $value3) {
-
-                $result = [$key, $key2, correcao($key3), $value3];
-
-                //var_dump($result);
-
+                $result = [$key, $key2 , correcao($key3), $value3];  
                 fputcsv($output, $result, ';');
             }
         }
@@ -40,6 +40,12 @@ function correcao($palavra)
     }
     return $palavra;
 
+}
+
+function ordena_mes($a, $b) {
+    $meses = array( 'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro' );
+    if ( array_search( $a, $meses) == array_search( $b, $meses) ) return 0;
+    return array_search( $a, $meses) > array_search( $b, $meses) ? 1 : -1;
 }
 
 outputCSV($array);

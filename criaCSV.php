003ab1ca287dc8368711b2430196a7a604041e39
Carlos Floresta <47724025+carlosfloresta@users.noreply.github.com>
2021-07-28 19:42:08 -0300

    Incluido Cabeçalho, havia esquecido

--- criaCSV.php
+++ criaCSV.php
@@ -4,6 +4,7 @@ include 'script.php';
 $array = include 'lista-de-compras.php';
 $filename = "data_export_" . date("d-m-Y") . ".csv";
 $output = fopen($filename, "w");
+fputcsv($output, ['Mes','Categoria','Produto','Quantidade'], ';');
 
 foreach (formataArray($array) as $value) {
     fputcsv($output, $value, ';');

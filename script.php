fddfa1ce20cad401aa3af6f462a219f24645db10
Carlos Floresta <47724025+carlosfloresta@users.noreply.github.com>
2021-07-28 13:57:15 -0300

    funcao correção

--- script.php
+++ script.php
@@ -11,7 +11,7 @@ function outputCSV($data)
         foreach ($value as $key2 => $value2) {
             foreach ($value2 as $key3 => $value3) {
 
-                $result = [$key, $key2, $key3, $value3];
+                $result = [$key, $key2, correcao($key3), $value3];
 
                 //var_dump($result);
 
@@ -20,7 +20,26 @@ function outputCSV($data)
         }
     }
     fclose($output);
-    die();  
+    die();
+}
+
+function correcao($palavra)
+{
+
+    if ($palavra == "Papel Hignico") {
+        return "Papel Higiênico";
+    }
+    if ($palavra == "Brocolis") {
+        return "Brócolis";
+    }
+    if ($palavra == "Chocolate ao leit") {
+        return "Chocolate ao leite";
+    }
+    if ($palavra == "Sabao em po") {
+        return "Sabão em pó";
+    }
+    return $palavra;
+
 }
 
 outputCSV($array);
